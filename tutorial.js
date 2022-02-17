ca7ec1929f14cb637976711a42ba8fe9bb2eea97
Nitish Rao <51140695+NITISH1-1@users.noreply.github.com>
2022-02-17 10:55:12 +0530

    JS tutorial

---
+++ tutorial.js
@@ -0,0 +1,13 @@
+/**
+ *
+ *      ``IMPORTANT: Everything in Javascript happens inside an execution Context``.
+ *
+ * It has two components:
+ * 1) Memory Component (Variables, key and values are stored, AKA known as VARIABLE COMPONENT).
+ * 2) Code Component (Thread of Execution: Line by line execution)
+ *
+ *      ``IMPORTANT: javascript is a synchronous single-threaded language
+ *                   (Execute one line at a time, once it is done executing, 
+ *                   it will move on to next line)``
+ *
+ * */
